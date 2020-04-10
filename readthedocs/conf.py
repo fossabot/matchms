4add18610f318417f3e2836b36ecb2a053851e50
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-11 00:01:19 +0200

    another exclude

--- readthedocs/conf.py
+++ readthedocs/conf.py
@@ -55,7 +55,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'readthedocs/conf.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'readthedocs/conf.rst', 'matchms/utils.py']
 
 
 # -- Options for HTML output -------------------------------------------------
